4380ef34421aabc493167cb75a86ea0b1df1f670
Tomasz 'Zen' Napierala <tomasz@napierala.org>
2016-06-20 23:45:34 +0200

    simplified implementation, added checksum calculation

--- pms3003/pms3003.ino
+++ pms3003/pms3003.ino
@@ -10,6 +10,32 @@
     PIN6  :RESET
     PIN7  :NC
     PIN8  :NC
+ Data format of PMS3003 is 24 one byte messages:
+ Header 1
+ Header 2
+ Control bit high = 2 x 9 + 2
+ Control bit low
+ PM1.0 high standard particle
+ PM1.0 low standard particle
+ PM2.5 high standard particle
+ PM2.5 low standard particle
+ PM10 high standard particle
+ PM10 low standard particle
+ PM1.0 high
+ PM1.0 low
+ PM2.5 high
+ PM2.5 low
+ PM10 high
+ PM10 low
+ Reserved 1 high
+ Reserved 1 low
+ Reserved 2 high
+ Reserved 2 low
+ Reserved 3 high
+ Reserved 4 low
+ Checksum high - sum of 9 previous readings
+ Checksum low
+ 
 
  In this example, we only use Serial to get PM 2.5 value.
 
@@ -27,46 +53,54 @@
 #define PMS_PIN_TX D6
 #define PMS_BAUDRATE 9600
 
-#define PMS_COMMAND1 0
-#define PMS_COMMAND2 1
-#define PMS_PM1C_HIGH 2
-#define PMS_PM1C_LOW 3
-#define PMS_PM25C_HIGH 4
-#define PMS_PM25C_LOW 5
-#define PMS_PM10C_HIGH 6
-#define PMS_PM10C_LOW 7
-#define PMS_PM1_HIGH 8
-#define PMS_PM1_LOW 9
-#define PMS_PM25_HIGH 10
-#define PMS_PM25_LOW 11
-#define PMS_PM10_HIGH 12
-#define PMS_PM10_LOW 13
-#define PMS_RES1_HIGH 14
-#define PMS_RES1_LOW 15
-#define PMS_RES2_HIGH 16
-#define PMS_RES2_LOW 17
-#define PMS_RES3_HIGH 18
-#define PMS_RES3_LOW 19
-#define PMS_CHECKSUM_HIGH 20
-#define PMS_CHECKSUM_LOW 21
+// Data format
+#define PMS_HEADER1 0 //0x42
+#define PMS_HEADER2 1 // 0x4d
+#define PMS_COMMAND1 2 // frame lenght high
+#define PMS_COMMAND2 3 // frame lenght low
+#define PMS_PM1C_HIGH 4
+#define PMS_PM1C_LOW 5
+#define PMS_PM25C_HIGH 6
+#define PMS_PM25C_LOW 7
+#define PMS_PM10C_HIGH 8
+#define PMS_PM10C_LOW 9
+#define PMS_PM1_HIGH 10
+#define PMS_PM1_LOW 11
+#define PMS_PM25_HIGH 12
+#define PMS_PM25_LOW 13
+#define PMS_PM10_HIGH 14
+#define PMS_PM10_LOW 15
+#define PMS_RES1_HIGH 16
+#define PMS_RES1_LOW 17
+#define PMS_RES2_HIGH 18
+#define PMS_RES2_LOW 19
+#define PMS_RES3_HIGH 20
+#define PMS_RES3_LOW 21
+#define PMS_CHECKSUM_HIGH 22
+#define PMS_CHECKSUM_LOW 23
 
 SoftwareSerial pms(PMS_PIN_RX, PMS_PIN_TX); // Initialize serial communiation with PMS 3003
 
-#define pmsDataLen 22
+#define pmsDataLen 24 // according to spec PMS3003 has 24 bytes long message
 uint8_t buf[pmsDataLen];
 int idx = 0;
-int pm10 = 0;
-int pm25 = 0;
+long pm10 = 0;
+long pm25 = 0;
 
 void setup() {
   Serial.begin(115200);
   pms.begin(PMS_BAUDRATE); // PMS 3003 UART has baud rate 9600
+  pinMode(PMS_PIN_SET, OUTPUT);
+  digitalWrite(PMS_PIN_SET, HIGH);
 }
 
 void loop() { // run over and over
   uint8_t c = 0;
   idx = 0;
   memset(buf, 0, pmsDataLen);
+
+  //digitalWrite(PMS_PIN_SET, HIGH);
+  delay(1000);
 
   while (true) {
     while (c != 0x42) {
@@ -87,10 +121,19 @@ void loop() { // run over and over
     while(!pms.available());
     buf[idx++] = pms.read();
   }
-
   long pms_checksum = word(buf[PMS_CHECKSUM_HIGH], buf[PMS_CHECKSUM_LOW]);
-  long pms_calc_checksum = 0x42 + 0x4d + buf[PMS_COMMAND2] + buf[PMS_COMMAND1] + buf[PMS_PM1C_LOW] + buf[PMS_PM25C_LOW] + buf[PMS_PM10C_LOW] + buf[PMS_PM1_LOW] + buf[PMS_PM25_LOW] + buf[PMS_PM10_LOW] + buf[PMS_RES1_LOW] + buf[PMS_RES2_LOW] + buf[PMS_RES3_LOW] +
-                                   buf[PMS_PM1C_HIGH] + buf[PMS_PM25C_HIGH] + buf[PMS_PM10C_HIGH] + buf[PMS_PM1_HIGH] + buf[PMS_PM25_HIGH] + buf[PMS_PM10_HIGH] + buf[PMS_RES1_HIGH] + buf[PMS_RES2_HIGH] + buf[PMS_RES3_HIGH];
+  long pms_calc_checksum = buf[PMS_HEADER1] + buf[PMS_HEADER2] + 
+                            buf[PMS_COMMAND1] + buf[PMS_COMMAND2] + 
+                            buf[PMS_PM1C_HIGH] + buf[PMS_PM1C_LOW] + 
+                            buf[PMS_PM25C_HIGH] + buf[PMS_PM25C_LOW] + 
+                            buf[PMS_PM10C_HIGH] + buf[PMS_PM10C_LOW] + 
+                            buf[PMS_PM1_HIGH] + buf[PMS_PM1_LOW] + 
+                            buf[PMS_PM25_HIGH] + buf[PMS_PM25_LOW] + 
+                            buf[PMS_PM10_HIGH] + buf[PMS_PM10_LOW] + 
+                            buf[PMS_RES1_HIGH] + buf[PMS_RES1_LOW] + 
+                            buf[PMS_RES2_HIGH] + buf[PMS_RES2_LOW] +
+                            buf[PMS_RES3_HIGH] + buf[PMS_RES3_LOW];
+
 
   if (pms_calc_checksum == pms_checksum) {
     Serial.print("Checksum correct: ");
@@ -103,21 +146,20 @@ void loop() { // run over and over
     Serial.print(pms_checksum);
     Serial.print(" / ");
     Serial.println(pms_calc_checksum);
+    Serial.println("Raw data dump:");
     for (int i=0; i< pmsDataLen;i++) {
       Serial.print(buf[i]);
-      Serial.print("..");
+      Serial.print(" ");
     }
     Serial.println();
     }
 
-  pm10 = ( buf[10] << 8 ) | buf[11];
-  pm25 = ( buf[12] << 8 ) | buf[13];
-
+  pm25 = word(buf[12], buf[13]);
+  pm10 = word(buf[14], buf[15]);
+ 
   Serial.print("pm2.5: ");
   Serial.print(pm25);
-  Serial.print(" ug/m3   ");
-  Serial.print("pm10: ");
-  Serial.print(pm10);
-  Serial.println(" ug/m3   ");
-  delay(5000);
+  Serial.print(" pm10: ");
+  Serial.println(pm10);
+  delay(4000);
 }
