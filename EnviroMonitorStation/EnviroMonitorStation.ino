bb1862e6f83d7ceeaa56587baea7874c3ab01a76
Krzysztof Hasiński <krzysztof.hasinski@gmail.com>
2017-03-26 14:56:39 +0200

    Adjust format of the endpoint

--- EnviroMonitorStation/EnviroMonitorStation.ino
+++ EnviroMonitorStation/EnviroMonitorStation.ino
@@ -12,7 +12,7 @@
 
 #include <ESP8266WiFi.h>
 #include <DNSServer.h>
-#include <ESP8266WebServer.h>00
+#include <ESP8266WebServer.h>
 #include <WiFiManager.h>
 
 #include <ArduinoJson.h>
@@ -34,9 +34,11 @@
 #define PMS_SET
 #define HEAT_PIN_SWITCH D4
 
-float target_temp = 30.0;
+#define HEAT_PIN_SWITCH D4
+#define TARGET_TEMP 30.0
 
-char apiEndpoint[130] = "http://app.smogly.pl/api/v1/station/cef6960f-6627-4c4f-9080-de6bbea41a7e/add-metering/?token=9539f7d1-d633-4838-81cb-6f8c712c6ad0"; //air monitoring API URL, default value
+char apiEndpoint[130] = "http://app.smogly.pl/api/v1/metering/";
+char token[130] = "9539f7d1-d633-4838-81cb-6f8c712c6ad0";
 
 SmoglyDHT dht;
 PMS3003 pms;
@@ -55,30 +57,23 @@ void setup() {
   pinMode(HEAT_PIN_SWITCH, OUTPUT);
   digitalWrite(HEAT_PIN_SWITCH, HIGH);
 
-  strcpy(apiEndpoint, config.apiEndpoint);
-
   config.read("/config.json");
 
-  WiFiManagerParameter custom_apiEndpoint("apiEndpoint", "API endpoint", apiEndpoint, 130);
+  WiFiManagerParameter custom_apiEndpoint("apiEndpoint", "Smogly URL", apiEndpoint, 130);
+  WiFiManagerParameter custom_token("token", "Token", token, 130);
 
   WiFiManager wifiManager;
   wifiManager.addParameter(&custom_apiEndpoint);
+  wifiManager.addParameter(&custom_token);
 
-  //fetches ssid and pass and tries to connect
-  //if it does not connect it starts an access point with the specified name
-  //and goes into a blocking loop awaiting configuration
   if (!wifiManager.autoConnect("Smogly Air Quality Monitor")) {
-    Serial.println("failed to connect and hit timeout");
+    Serial.println("Failed to connect and hit timeout");
     delay(3000);
     ESP.reset();
     delay(5000);
   }
 
-  //if you get here you have connected to the WiFi
   Serial.println("Successfully connected to WiFi network");
-
-  //read updated parameters
-  //strcpy(config.apiEndpoint, custom_apiEndpoint.getValue());
 
   config.save("/config.json");
 
@@ -108,7 +103,7 @@ void loop() {
       return;
     }
 
-    if (t >= target_temp) // we're going to measure humidity instaead temp
+    if (t >= TARGET_TEMP) // we're going to measure humidity instead of temp
     {
       digitalWrite(HEAT_PIN_SWITCH, LOW);
       Serial.print(t);
@@ -121,7 +116,7 @@ void loop() {
 
     String output = createPayload(h,t, pm25, pm10);
     HTTPClient http;
-    http.begin("http://app.smogly.pl/api/v1/station/cef6960f-6627-4c4f-9080-de6bbea41a7e/add-metering/?token=9539f7d1-d633-4838-81cb-6f8c712c6ad0");
+    http.begin(apiEndpoint);
     http.addHeader("Content-Type", "application/json");
     http.POST(output);
     http.end();
@@ -133,19 +128,21 @@ String createPayload(float h, float t, long pm25, long pm10)
 {
   StaticJsonBuffer<300> jsonBuffer;
   JsonObject& root = jsonBuffer.createObject();
-  root["pm01"] = "0";
+  root["pm01"] = 0;
   root["pm25"] = pm25;
   root["pm10"] = pm10;
   root["temp_out1"] = t;
-  root["temp_out2"] = "0";
-  root["temp_out3"] = "0";
-  root["temp_int_air1"] = "0";
+  root["temp_out2"] = 0;
+  root["temp_out3"] = 0;
+  root["temp_int_air1"] = 0;
   root["hum_out1"] = h;
-  root["hum_out2"] = "0";
-  root["hum_out3"] = "0";
-  root["hum_int_air1"] = "0";
-  root["rssi"] = "0";
-  root["bpress_out1"] = "0";
+  root["hum_out2"] = 0;
+  root["hum_out3"] = 0;
+  root["hum_int_air1"] = 0;
+  root["rssi"] = 0;
+  root["bpress_out1"] = 0;
+  root["hw_id"] = "0";
+  root["token"] = token;
   String output;
   root.printTo(output);
   return output;
